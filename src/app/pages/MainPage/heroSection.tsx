4396d8d8cf13df1e849d1fa2790b07140eb38e79
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-09-19 09:35:43 +0530

    added basic styles and text to hero section

--- src/app/pages/MainPage/heroSection.tsx
+++ src/app/pages/MainPage/heroSection.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const HeroSection = () => {
     return(
-        <div>
-            <h1>Hero Section</h1>
+        <div className="bg-gradient-to-bl from-blue-500 via-purple-500 to-pink-500 h-screen flex items-center justify-center">
+            <h1 className="text-white text-5xl font-bold">
+                Welcome to the Hero Section
+            </h1>
         </div>
-    )
+    );
 }
 
 export default HeroSection;
