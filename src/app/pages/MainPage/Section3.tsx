82c8e314cb923b975f9f9f5c0f77498437ead9b3
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-09-21 14:57:09 +0530

    created the 3 rd section

---
+++ src/app/pages/MainPage/Section3.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Section3 = () => {
+    return(
+        <div className="bg-gradient-to-b from-slate-800 via-slate-700 to-slate-700 h-screen flex items-center justify-center">
+            <h1 className="text-slate-500 text-5xl font-bold">
+                Welcome to the 3rd Section
+            </h1>
+        </div>
+    );
+}
+
+export default Section3;
