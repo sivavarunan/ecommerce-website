795b22118fa13cacb98a0de9750dbdf17ab0247b
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-09-22 13:17:09 +0530

    added bacground lines component

---
+++ src/app/pages/MainPage/MainSection.tsx
@@ -0,0 +1,20 @@
+import { BackgroundLines } from "@/app/components/ui/background-lines";
+import React from "react";
+
+
+const MainSection = () => {
+    return (
+        <div>
+            <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
+                <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+                    Welcome to my , <br /> Web Store.
+                </h2>
+                <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
+                    Feel free to browse and buy products
+                </p>
+            </BackgroundLines>
+        </div>
+    )
+}
+
+export default MainSection;
--- src/app/pages/MainPage/page.tsx
+++ src/app/pages/MainPage/page.tsx
@@ -1,13 +1,11 @@
 'use client'
 import React from "react";
-import Link from "next/link";
 import { NavbarComponent } from "@/app/components/ui/NavBar";
-import { motion } from "framer-motion";
-import { LampContainer } from "@/app/components/ui/lamp";
-
 import Section2 from "./section2";
 import Section3 from "./Section3";
 import { ImageSliderComp } from "./heroSection";
+// import Section1 from "./section1";
+import MainSection from "./MainSection";
 
 const HomePage = () => {
     return (
@@ -15,25 +13,9 @@ const HomePage = () => {
             <div className="">
                 <NavbarComponent />
             </div>
-            <LampContainer className="mt-20">
-                <motion.div
-                    initial={{ opacity: 0.5, y: 100 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{
-                        delay: 0.3,
-                        duration: 0.8,
-                        ease: "easeInOut",
-                    }}
-                    className="top-10 dark:bg-gradient-to-br bg-cyan-900 from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
-                >
-                    Welcome <br /> to the website <br />
-                    <Link href="/shop">
-                        <button className="mt-6 px-6 py-2 text-base font-semibold text-white bg-cyan-600 rounded-full shadow hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
-                            Shop
-                        </button>
-                    </Link>
-                </motion.div>
-            </LampContainer>
+            <div>
+                <MainSection />
+            </div>
             <div>
                 <ImageSliderComp />
             </div>
