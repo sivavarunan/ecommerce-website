bdaa90813450fc5d214f6c017d1ed7d537b2279b
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-10-06 14:41:32 +0530

    ..

--- src/app/context/CardContext/page.tsx
+++ src/app/context/CardContext/page.tsx
@@ -17,7 +17,7 @@ const CartPage = () => {
 
   return (
     <div className="flex items-center justify-center h-screen dark:bg-gradient-to-tr from-slate-800 via-slate-900 to-slate-950">
-      <div className="w-full max-w-md p-6 bg-slate-900 rounded-lg shadow-lg flex flex-col">
+      <div className="w-full max-w-lg p-6 bg-slate-900 rounded-lg shadow-lg flex flex-col">
         <h1 className="text-3xl font-bold text-center mb-6 text-gray-100">Your Cart</h1>
 
         {/* Scrollable area for cart items */}
@@ -25,7 +25,7 @@ const CartPage = () => {
           {cartItems.length === 0 ? (
             <p className="text-gray-400 text-center">Your cart is empty. Start adding items to your cart!</p>
           ) : (
-            <div className="space-y-4">
+            <div className="space-y-4 p-4">
               <ul className="space-y-2">
                 {cartItems.map((item) => (
                   <li key={item.id} className="flex items-center justify-between p-4 rounded-lg shadow-lg transition-transform transform hover:scale-105 bg-slate-800">
